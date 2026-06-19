0074fc7510056f831db084636c819a087779c58c
Kairav <kairavdutta@gmail.com>
2026-06-19 13:16:06 +0530

    added the emit function: it increases bytecode capacity if size = capacity else it just adds the instruction to the array code

--- src/bytecode.c
+++ src/bytecode.c
@@ -8,4 +8,14 @@ static Bytecode* createBytecode(Arena* ar){
     return bc;
 }
 
-
+static void emit(Arena* ar, Bytecode* bc, Instr i){
+    if (bc->count >= bc->capacity){
+        Instr* old = bc->code;
+        int newCap = bc->capacity * 2;
+        Instr* newGrown = (Instr*)arena_alloc(ar, sizeof(Instr) * newCap);
+        memcpy(newGrown, old, sizeof(Instr) * bc->count);
+        bc->code = newGrown;
+        bc->capacity = newCap;
+    }
+    bc->code[bc->count++] = i;
+}
