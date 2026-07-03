caa907b5e99a1c99b76e3e817137b7e92b1eed61
Kairav <kairavdutta@gmail.com>
2026-07-03 15:54:35 +0530

    declared loadbinary in json parser

--- include/jsonParser.h
+++ include/jsonParser.h
@@ -15,6 +15,7 @@ typedef enum {
 yyjson_doc* parseJSON(const char*); 
 
 RuleEngine* build_ast(yyjson_doc*, FactDB*, ActionEntry*);
+RuleEngine* loadBytecode(const char* file, FactDB* db);
 
 static Node* build_node(Arena*, FactDB*, yyjson_val*);
 static Node* build_and_or(Arena*, FactDB*, yyjson_val*, Type);
