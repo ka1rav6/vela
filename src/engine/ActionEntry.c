f5d3c8c6f69cfcc2beed362ae6f68a5c0e64a5b7
Kairav <kairavdutta@gmail.com>
2026-07-03 16:22:10 +0530

    added documentation

--- src/engine/ActionEntry.c
+++ src/engine/ActionEntry.c
@@ -1,6 +1,7 @@
 #include "../../include/ActionEntry_internal.h"
 
-int registerAction(ActionEntry** g_registry, const char* action, Action_f func, void* ctx) {
+// registers the action in the registry
+int registerAction(ActionEntry** registry, const char* action, Action_f func, void* ctx) {
     if (strlen(action) > MAX_ACTION_NAME){
         fprintf(stderr, "Cannot have an action name that exceeds the limit of %d letters: %s\n", MAX_ACTION_NAME, action);
         return -1;
@@ -14,28 +15,32 @@ int registerAction(ActionEntry** g_registry, const char* action, Action_f func, 
     strcpy(e->action, action);
     e->func = func;
     e->ctx  = ctx;
-    HASH_ADD_STR(*g_registry, action, e);
+    HASH_ADD_STR(*registry, action, e); // uthash macro to add the action to the registry hashmap
     return 0;
 }
 
-ActionEntry* lookupAction(ActionEntry* g_registry, const char* action) {
+// searches through the hashmapped registry to find the action based on the name
+ActionEntry* lookupAction(ActionEntry* registry, const char* action) {
     ActionEntry* e;
-    HASH_FIND_STR(g_registry, action, e);
+    HASH_FIND_STR(registry, action, e);
     return e;
 }
 
-void freeRegistry(ActionEntry** g_registry) {
+// frees the actionEntry registry
+void freeRegistry(ActionEntry** registry) {
     ActionEntry *e, *tmp;
-    HASH_ITER(hh, *g_registry, e, tmp) {
-        HASH_DEL(*g_registry, e);
+    HASH_ITER(hh, *registry, e, tmp) {
+        HASH_DEL(*registry, e);
         free(e);
     }
 }
 
+// returns the function of the action entry if it exists
 Action_f action_entry_func(const ActionEntry* e) {
     return e ? e->func : NULL;
 }
 
+// returns the context of the function of the action entry if it exists
 void* action_entry_ctx(const ActionEntry* e) {
     return e ? e->ctx : NULL;
 }
