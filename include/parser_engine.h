82102a4a4ad15c207020fcf301594eb8b65046ac
Kairav <kairavdutta@gmail.com>
2026-07-03 18:46:00 +0530

    removed static function declarations and moved forward declarations to c file

--- include/parser_engine.h
+++ include/parser_engine.h
@@ -17,9 +17,4 @@ yyjson_doc* parseJSON(const char*);
 RuleEngine* build_ast(yyjson_doc*, FactDB*, ActionEntry*);
 RuleEngine* loadBytecode(const char* file, FactDB* db);
 
-static Node* build_node(Arena*, FactDB*, yyjson_val*);
-static Node* build_and_or(Arena*, FactDB*, yyjson_val*, Type);
-static Node* build_fact(Arena*, FactDB*, yyjson_val*);
-static Node* build_not(Arena*, FactDB*, yyjson_val*);
-static Node* build_compare(Arena*, FactDB*, const char*, yyjson_val*);
-static void build_factdb(FactDB*, yyjson_val*);
+
