b7c6b95f4423690a30f9d4aabb7bd3b12b9e623d
Kairav <kairavdutta@gmail.com>
2026-07-07 12:38:43 +0530

    included all files and created main function

---
+++ test/unit_test.c
@@ -0,0 +1,34 @@
+#include "greatest.h"
+
+#include "../include/arena.h"
+#include "../include/arena_internal.h"
+#include "../include/factdb.h"
+#include "../include/factdb_internal.h"
+#include "../include/ConditionTree.h"
+#include "../include/bytecode.h"
+#include "../include/ActionEntry.h"
+#include "../include/ActionEntry_internal.h"
+#include "../include/rule.h"
+#include "../include/rule_internal.h"
+#include "../include/engine.h"
+#include "../include/engine_internal.h"
+#include "../include/parser_engine.h"
+#include "../include/semanticChecker.h"
+
+#include <math.h>
+#include <float.h>
+
+
+
+
+
+
+
+// ---------------------- MAIN ------------------------//
+GREATEST_MAIN_DEFS()
+
+int main(int argc, char** argv)
+{
+    GREATEST_MAIN_BEGIN;
+    GREATEST_MAIN_END;
+}
